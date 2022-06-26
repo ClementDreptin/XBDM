cba4b2ff899f62b6a2d96231fa91fcad0181323d
Clément Dreptin <cdreptin@gmail.com>
2022-06-26 17:48:17 +0200

    Stopped sorting includes to prevent the precompiled header from being moved + formatted all files with the clang-format config

--- src/Console.h
+++ src/Console.h
@@ -2,50 +2,52 @@
 
 #include "Definitions.h"
 
-
 namespace XBDM
 {
-    class Console
-    {
-    public:
-        Console();
-        Console(const std::string &ipAddress);
 
-        bool OpenConnection();
-        bool CloseConnection();
+class Console
+{
+public:
+    Console();
+    Console(const std::string &ipAddress);
 
-        std::string GetName();
-        std::vector<Drive> GetDrives();
-        std::set<File> GetDirectoryContents(const std::string &directoryPath);
+    bool OpenConnection();
+    bool CloseConnection();
 
-        void LaunchXEX(const std::string &xexPath);
+    std::string GetName();
+    std::vector<Drive> GetDrives();
+    std::set<File> GetDirectoryContents(const std::string &directoryPath);
 
-        void ReceiveFile(const std::string &remotePath, const std::string &localPath);
-        void SendFile(const std::string &remotePath, const std::string &localPath);
-        void DeleteFile(const std::string &path, bool isDirectory);
-        void CreateDirectory(const std::string &path);
-        void RenameFile(const std::string &oldName, const std::string &newName);
+    void LaunchXEX(const std::string &xexPath);
 
-        bool IsConnected() { return m_Connected; }
-    private:
-        bool m_Connected = false;
-        std::string m_IpAddress;
-        SOCKET m_Socket;
-        static const int s_PacketSize = 2048;
+    void ReceiveFile(const std::string &remotePath, const std::string &localPath);
+    void SendFile(const std::string &remotePath, const std::string &localPath);
+    void DeleteFile(const std::string &path, bool isDirectory);
+    void CreateDirectory(const std::string &path);
+    void RenameFile(const std::string &oldName, const std::string &newName);
 
-        std::string Receive();
-        void SendCommand(const std::string &command);
+    bool IsConnected() { return m_Connected; }
 
-        std::vector<std::string> SplitResponse(const std::string &response, const std::string &delimiter);
-        bool EndsWith(const std::string &line, const std::string &ending);
+private:
+    bool m_Connected = false;
+    std::string m_IpAddress;
+    SOCKET m_Socket;
+    static const int s_PacketSize = 2048;
 
-        DWORD GetIntegerProperty(const std::string &line, const std::string &propertyName, bool hex = true);
-        std::string GetStringProperty(const std::string &line, const std::string &propertyName);
+    std::string Receive();
+    void SendCommand(const std::string &command);
 
-        void ClearSocket();
-        void CleanupSocket();
-        void CloseSocket();
+    std::vector<std::string> SplitResponse(const std::string &response, const std::string &delimiter);
+    bool EndsWith(const std::string &line, const std::string &ending);
 
-        void SleepFor(DWORD milliseconds);
-    };
+    DWORD GetIntegerProperty(const std::string &line, const std::string &propertyName, bool hex = true);
+    std::string GetStringProperty(const std::string &line, const std::string &propertyName);
+
+    void ClearSocket();
+    void CleanupSocket();
+    void CloseSocket();
+
+    void SleepFor(DWORD milliseconds);
+};
+
 }
